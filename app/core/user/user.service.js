6a2da5be49e375b05b5dbc1dcb72cc8ac80520a7
Caitlin Flattery <caitlinflattery@gmail.com>
2017-06-14 12:44:58 -0500

    created https request service to get user info using factory function

--- app/core/user/user.service.js
+++ app/core/user/user.service.js
@@ -1,3 +1,13 @@
 /* global angular */
 
-console.log(TEST)
+angular.module('core.user')
+  .factory('User', ['$resource',
+    function ($resource) {
+      return $resource('https://api.github.com/users/:username/repos?access_token=' + TOKEN, {},
+        {
+          query: {
+            method: 'GET',
+            params: {username: 'cflatts'}
+          }
+        })
+    }])
