fb93992981b006c2539c5bbe1afd528e74ef79de
Caitlin Flattery <caitlinflattery@gmail.com>
2017-06-14 12:47:10 -0500

    created controller function for user info

--- app/userInfo/user-info.component.js
+++ app/userInfo/user-info.component.js
@@ -2,5 +2,10 @@
 
 angular.module('userInfo')
   .component('userInfo', {
-    templateUrl: 'userInfo/user-info.template.html'
+    templateUrl: 'userInfo/user-info.template.html',
+    controller: ['User',
+      function UserInfoController (User) {
+        this.user = User.query()
+      }
+    ]
   })
--- app/userInfo/user-info.module.js
+++ app/userInfo/user-info.module.js
@@ -1,3 +1,3 @@
 /* global angular */
 
-angular.module('userInfo', [])
+angular.module('userInfo', ['core.user'])
